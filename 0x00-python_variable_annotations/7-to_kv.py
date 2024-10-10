25e2e80dc574072ba7bb24e8fc9e3f3909d06fcf
mohamed271220 <mohamedaprahym@gmail.com>
2024-10-10 14:11:14 +0300

    s

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -8,6 +8,5 @@ from typing import Union, Tuple
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Returns a tuple where the first element is the string k
-    and the second element is the square of the int/float v.
     """
     return (k, float(v**2))
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -8,6 +8,5 @@ from typing import Iterable, Sequence, List, Tuple
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Returns a list of tuples, where each tuple contains an element from the input
-    iterable and its length.
     """
     return [(i, len(i)) for i in lst]
