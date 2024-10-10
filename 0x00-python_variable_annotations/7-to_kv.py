7aceec98d573c2ba2cd00009f92a08328d9cb9e2
mohamed271220 <mohamedaprahym@gmail.com>
2024-10-10 14:14:11 +0300

    s

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 """
-This module provides a function to create a tuple from a string and an int/float.
+This module provides a function to create tuple
 """
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Converts a key and its value to a tuple of the key and
-    the square of its value.
-    """
+    """Converts a key and its value to a tuple of the key"""
     return (k, float(v**2))
